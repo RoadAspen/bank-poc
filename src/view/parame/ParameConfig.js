8a273f41f1a5772f40f713f39f7d73f5b3a77e42
celi <qceli@foxmail.com>
2019-09-11 17:43:16 +0800

    用户管理 参数配置

--- src/view/parame/ParameConfig.js
+++ src/view/parame/ParameConfig.js
@@ -2,16 +2,324 @@
  *  参数配置页面
  */
 
- import React from 'react';
+import React from 'react';
+import { Form, Select, Button, Tabs, Comment, List, Input, Col, Row, Tag, Tooltip, Icon } from 'antd';
+// import moment from 'moment';
 
- class ParameConfig extends React.Component{
-     render(){
-         return (
-             <div>
-                 参数配置页面
-             </div>
-         )
-     }
- }
+const InputGroup = Input.Group;
+const { Option } = Select;
+const { TabPane } = Tabs;
 
- export default ParameConfig;
+const { TextArea } = Input;
+
+const CommentList = ({ comments }) => (
+    <List
+        dataSource={comments}
+        itemLayout="horizontal"
+        renderItem={props => (
+            <List.Item>
+                <List.Item.Meta
+                    title={<div style={{ display: 'block !imporatant', flex: 0 }}>话术{props.id + 1}：</div>}
+                />
+                {props.content}
+                {/* <Comment {...props} /> */}
+            </List.Item>
+        )}
+    />
+);
+
+const Editor = ({ onChange, onSubmit, submitting, value }) => (
+    <div>
+        <Form.Item>
+            <TextArea rows={4} onChange={onChange} value={value} />
+        </Form.Item>
+        <Form.Item>
+            <Button htmlType="submit" loading={submitting} onClick={onSubmit} type="primary">
+                增加话术
+        </Button>
+        </Form.Item>
+    </div>
+);
+
+const formItemLayout = {
+    labelCol: {
+        xs: { span: 24 },
+        sm: { span: 5 },
+    },
+    wrapperCol: {
+        xs: { span: 24 },
+        sm: { span: 12 },
+    },
+};
+
+class ParameConfig extends React.Component {
+    state = {
+        comments: [{
+            id: 0,
+            content: 'xxx用户，您现有本行信用卡首刷礼暂未领取，详情请登录官网查看。'
+        }, {
+            id: 1,
+            content: '测试话术'
+        }
+        ],
+        submitting: false,
+        value: '',
+        tags: ['公众号1', '公众号2', '公众号3'],
+        inputVisible: false,
+        inputValue: '',
+        periodTags: ['按周', '按月', '按季度', '实时'],
+        periodInputVisible: false,
+        periodInputValue: '',
+        areaTags: ['小型（100 人以内）', '中型（1000 人）', '大型（1000+ 人）']
+    };
+
+    render() {
+        const { tags, inputVisible, inputValue } = this.state;
+        const { periodTags, periodInputVisible, periodInputValue, areaTags } = this.state;
+        return (
+            <>
+                <Form {...formItemLayout}>
+                    <div>
+                        {/* <div>渠道</div> */}
+                        <Tabs defaultActiveKey="1">
+                            <TabPane tab="渠道" key="1">
+                                <Form.Item label="微信公众号">
+                                    {tags.map((tag, index) => {
+                                        const isLongTag = tag.length > 20;
+                                        const tagElem = (
+                                            <Tag key={tag} closable={index !== 0} onClose={() => this.handleClose(tag)}>
+                                                {isLongTag ? `${tag.slice(0, 20)}...` : tag}
+                                            </Tag>
+                                        );
+                                        return isLongTag ? (
+                                            <Tooltip title={tag} key={tag}>
+                                                {tagElem}
+                                            </Tooltip>
+                                        ) : (
+                                                tagElem
+                                            );
+                                    })}
+                                    {inputVisible && (
+                                        <Input
+                                            ref={this.saveInputRef}
+                                            type="text"
+                                            size="small"
+                                            style={{ width: 78 }}
+                                            value={inputValue}
+                                            onChange={this.handleInputChange}
+                                            onBlur={this.handleInputConfirm}
+                                            onPressEnter={this.handleInputConfirm}
+                                        />
+                                    )}
+                                    {!inputVisible && (
+                                        <Tag onClick={this.showInput} style={{ background: '#fff', borderStyle: 'dashed' }}>
+                                            <Icon type="plus" /> 新增微信公众号
+                                        </Tag>
+                                    )}
+                                </Form.Item>
+                                <Form.Item label="短信">
+                                    <InputGroup size="small">
+                                        <Row gutter={8}>
+                                            {/* <Col span={5}>
+                                                <Input defaultValue="0571" />
+                                            </Col> */}
+                                            <Col span={10}>
+                                                <Input defaultValue="1069800095313" />
+                                            </Col>
+                                        </Row>
+                                    </InputGroup>
+                                </Form.Item>
+                                <Form.Item label="客服电销">
+                                    <InputGroup size="small">
+                                        <Row gutter={10}>
+                                            {/* <Col span={5}>
+                                                <Input defaultValue="0571" />
+                                            </Col> */}
+                                            <Col span={8}>
+                                                <Input defaultValue="95313" />
+                                            </Col>
+                                        </Row>
+                                    </InputGroup>
+                                </Form.Item>
+                                <div style={{ textAlign: "center" }}>
+                                    <Button type="primary" size="large">保存</Button>
+                                </div>
+                            </TabPane>
+                            <TabPane tab="卡产品参数" key="2">
+                                <Form.Item label="卡类型">
+                                    <Select defaultValue="1">
+                                        <Option value="1">白金卡</Option>
+                                        <Option value="2">金卡</Option>
+                                        <Option value="3">普卡</Option>
+                                    </Select>
+                                </Form.Item>
+                                <Form.Item label="卡种">
+                                    <Select defaultValue="1">
+                                        <Option value="1">太阳全利卡</Option>
+                                        <Option value="2">太阳全飞卡</Option>
+                                        <Option value="3">太阳全乐卡</Option>
+                                        <Option value="3">太阳全行卡</Option>
+                                    </Select>
+                                </Form.Item>
+                                <div style={{ textAlign: "center" }}>
+                                    <Button type="primary" size="large">保存</Button>
+                                </div>
+                            </TabPane>
+                            <TabPane tab="市场活动" key="3">
+                                <Form.Item label="周期">
+                                {periodTags.map((tag, index) => {
+                                        const isLongTag = tag.length > 20;
+                                        const tagElem = (
+                                            <Tag key={tag} closable={index !== 0} onClose={() => this.handleClose(tag)}>
+                                                {isLongTag ? `${tag.slice(0, 20)}...` : tag}
+                                            </Tag>
+                                        );
+                                        return isLongTag ? (
+                                            <Tooltip title={tag} key={tag}>
+                                                {tagElem}
+                                            </Tooltip>
+                                        ) : (
+                                                tagElem
+                                            );
+                                    })}
+                                    {periodInputVisible && (
+                                        <Input
+                                            ref={this.saveInputRef}
+                                            type="text"
+                                            size="small"
+                                            style={{ width: 78 }}
+                                            value={periodInputValue}
+                                            onChange={this.handleInputChange}
+                                            onBlur={this.handleInputConfirm}
+                                            onPressEnter={this.handleInputConfirm}
+                                        />
+                                    )}
+                                    {!periodInputVisible && (
+                                        <Tag onClick={this.showInput} style={{ background: '#fff', borderStyle: 'dashed' }}>
+                                            <Icon type="plus" /> 新增周期
+                                        </Tag>
+                                    )}
+                                </Form.Item>
+                                <Form.Item label="规模">
+                                {areaTags.map((tag, index) => {
+                                        const isLongTag = tag.length > 20;
+                                        const tagElem = (
+                                            <Tag key={tag} closable={index !== 0} onClose={() => this.handleClose(tag)}>
+                                                {isLongTag ? `${tag.slice(0, 20)}...` : tag}
+                                            </Tag>
+                                        );
+                                        return isLongTag ? (
+                                            <Tooltip title={tag} key={tag}>
+                                                {tagElem}
+                                            </Tooltip>
+                                        ) : (
+                                                tagElem
+                                            );
+                                    })}
+                                    {periodInputVisible && (
+                                        <Input
+                                            ref={this.saveInputRef}
+                                            type="text"
+                                            size="small"
+                                            style={{ width: 78 }}
+                                            value={periodInputValue}
+                                            onChange={this.handleInputChange}
+                                            onBlur={this.handleInputConfirm}
+                                            onPressEnter={this.handleInputConfirm}
+                                        />
+                                    )}
+                                    {!periodInputVisible && (
+                                        <Tag onClick={this.showInput} style={{ background: '#fff', borderStyle: 'dashed' }}>
+                                            <Icon type="plus" /> 新增规模
+                                        </Tag>
+                                    )}
+                                </Form.Item>
+                                <div style={{ textAlign: "center" }}>
+                                    <Button type="primary" size="large">保存</Button>
+                                </div>
+                            </TabPane>
+                            <TabPane tab="营销话术" key="4">
+                                {this.state.comments.length > 0 && <CommentList comments={this.state.comments} />}
+                                <Comment
+                                    content={
+                                        <Editor
+                                            onChange={this.handleChange}
+                                            onSubmit={this.handleSubmit}
+                                            submitting={this.state.submitting}
+                                            value={this.state.value}
+                                        />
+                                    }
+                                />
+                            </TabPane>
+                        </Tabs>
+                    </div>
+
+                    {/* <Button type="danger" size="large">取消</Button> */}
+                </Form>
+            </>
+        )
+    }
+
+    handleSubmit = () => {
+        if (!this.state.value) {
+            return;
+        }
+
+        this.setState({
+            submitting: true,
+        });
+
+        setTimeout(() => {
+            this.setState({
+                submitting: false,
+                value: '',
+                comments: [
+                    ...this.state.comments,
+                    {
+                        id: this.state.comments.length,
+                        content: this.state.value
+                    }
+                ],
+            });
+        }, 1000);
+    };
+
+    handleChange = e => {
+        this.setState({
+            value: e.target.value,
+        });
+    };
+
+
+    handleClose = removedTag => {
+        const tags = this.state.tags.filter(tag => tag !== removedTag);
+        console.log(tags);
+        this.setState({ tags });
+    };
+
+    showInput = () => {
+        this.setState({ inputVisible: true }, () => this.input.focus());
+    };
+
+    handleInputChange = e => {
+        this.setState({ inputValue: e.target.value });
+    };
+
+    handleInputConfirm = () => {
+        const { inputValue } = this.state;
+        let { tags } = this.state;
+        if (inputValue && tags.indexOf(inputValue) === -1) {
+            tags = [...tags, inputValue];
+        }
+        console.log(tags);
+        this.setState({
+            tags,
+            inputVisible: false,
+            inputValue: '',
+        });
+    };
+
+    saveInputRef = input => (this.input = input);
+}
+
+export default ParameConfig;
--- src/view/system/SystemManage.js
+++ src/view/system/SystemManage.js
@@ -2,16 +2,254 @@
  * 系统用户管理
  */
 
- import React from 'react';
+import React from 'react';
+import { Tabs, Table, Row, Col, Input, Select, Button, Modal, Form, message } from 'antd';
 
- class SystemManage extends React.Component{
-     render(){
-         return (
-             <div>
-                 系统用户管理页面
-             </div>
-         )
-     }
- }
+const { TabPane } = Tabs;
+const { Option } = Select;
+const formItemLayout = {
+    labelCol: {
+        xs: { span: 24 },
+        sm: { span: 5 },
+    },
+    wrapperCol: {
+        xs: { span: 24 },
+        sm: { span: 12 },
+    },
+};
 
- export default SystemManage;
+class SystemManage extends React.Component {
+    state = {
+        visible: false,
+        operation: '',
+        editData: {
+            name: '',
+            sex: '',
+            role: '',
+            jurisdiction: '',
+            email: ''
+        },
+        changeName: '',
+        changeRole: '',
+        changeEmail: ''
+    };
+
+    render() {
+        var that = this
+        const peopledataSource = [
+            {
+                key: 0,
+                name: '系统管理员',
+                sex: '男',
+                role: '系统管理员',
+                jurisdiction: [' 用户管理', ' 参数配置', ' 营销活动管理', ' 统计分析'],
+                email: 'admin@kq300061.com'
+            },
+            {
+                key: 0,
+                name: '王占朋',
+                sex: '男',
+                role: '用户管理员',
+                jurisdiction: [' 用户管理'],
+                email: 'zhanpeng.wang@kq300061.com'
+            },
+            {
+                key: 1,
+                name: '孔甲',
+                sex: '女',
+                role: '活动管理员',
+                jurisdiction: [' 参数配置', ' 营销活动管理', ' 统计分析'],
+                email: 'jia.kong@kq300061.com'
+            }
+        ];
+        const peoplecolumns = [
+            {
+                title: '用户名',
+                dataIndex: 'name',
+                key: 'name',
+            },
+            {
+                title: '性别',
+                dataIndex: 'sex',
+                key: 'sex',
+            },
+            {
+                title: '角色',
+                dataIndex: 'role',
+                key: 'role',
+            },
+            {
+                title: '权限',
+                dataIndex: 'jurisdiction',
+                key: 'jurisdiction',
+            },
+            {
+                title: '邮箱',
+                dataIndex: 'email',
+                key: 'email',
+            },
+            {
+                title: '操作',
+                dataIndex: 'deal',
+                key: 'deal',
+                render: (text, item) => {
+                    //注意 this 为 render 方法内部的this
+                    return <><Button type="primary" size="small" onClick={() => { return that.handleUpdate(item) }} style={{ marginRight: 10 }}>更新</Button>
+                        <Button type="danger" size="small" onClick={() => { return that.handleDelete(item) }}>删除</Button>
+                    </>
+                }
+            }
+        ];
+        return (
+            <>
+                <Tabs defaultActiveKey="1" onChange={this.callback}>
+                    <TabPane tab="用户管理" key="1">
+                        <Row>
+                            <Col span={24}>
+                                <span style={{ marginRight: 10 }}>用户名</span>
+                                <Input placeholder="输入用户名" value={this.state.changeName} onChange={this.changeNameInfo} style={{ width: 200, marginRight: 10 }} />
+                                <span style={{ marginRight: 10 }}>角色</span>
+                                <Select allowClear={true} value={this.state.changeRole} onChange={this.changeRoleInfo.bind(this, 'role')} style={{ width: 200, marginRight: 10 }}>
+                                    <Option value="系统管理员">系统管理员</Option>
+                                    <Option value="用户管理员">用户管理员</Option>
+                                    <Option value="活动管理员">活动管理员</Option>
+                                </Select>
+                                <span style={{ marginRight: 10 }}>邮箱</span>
+                                <Input placeholder="输入邮箱账号" value={this.state.changeEmail} onChange={this.changeEmailInfo} style={{ width: 200, marginRight: 10 }} />
+                                <Button type="primary" onClick={this.SelectInfo}>检索</Button>
+                                <Button type="primary" style={{ marginLeft: 10 }} onClick={this.addUser}>新增</Button>
+                            </Col>
+                        </Row>
+                        <Table dataSource={peopledataSource} columns={peoplecolumns} style={{ marginTop: 10 }} bordered={true} />
+                    </TabPane>
+                </Tabs>
+                <div>
+                    <Modal
+                        title={this.state.operation}
+                        visible={this.state.visible}
+                        onOk={this.hideModal}
+                        onCancel={this.hideModal}
+                        okText="确认"
+                        cancelText="取消"
+                    >
+                        <Form {...formItemLayout}>
+                            <Form.Item {...formItemLayout} label="用户姓名">
+                                <Input placeholder="请输入用户姓名" value={this.state.editData.name} />
+                            </Form.Item>
+                            <Form.Item {...formItemLayout} label="性别">
+                                <Select placeholder='请选择性别' value={this.state.editData.sex}>
+                                    <Option value="男">男</Option>
+                                    <Option value="女">女</Option>
+                                </Select>
+                            </Form.Item>
+                            <Form.Item {...formItemLayout} label="角色">
+                                {/* <Input placeholder="请输入角色" value={this.state.editData.role} /> */}
+                                <Select placeholder='请选择角色' value={this.state.editData.role}>
+                                    <Option value="系统管理员">系统管理员</Option>
+                                    <Option value="用户管理员">用户管理员</Option>
+                                    <Option value="活动管理员">活动管理员</Option>
+                                </Select>
+                            </Form.Item>
+                            <Form.Item {...formItemLayout} label="权限">
+                                <Input placeholder="请输入权限" value={this.state.editData.jurisdiction} />
+                            </Form.Item>
+                            <Form.Item {...formItemLayout} label="邮箱">
+                                <Input placeholder="请输入邮箱账户" value={this.state.editData.email} />
+                            </Form.Item>
+                        </Form>
+                    </Modal>
+                    {/* <WrappedDynamicRule /> */}
+                </div>
+            </>
+        )
+    }
+    changeNameInfo = (e) => {
+        // console.log(item)
+        // console.log(e.target.value)
+        // for(let StateItem in this.state.searchInfo){
+        //     console.log(item)
+        //     console.log(StateItem)
+        //     if(item === StateItem){
+        //         console.log(StateItem)
+        //         let data = Object.assign({}, this.state.datavalue, {
+        //             [StateItem]: e.target.value
+        //         })
+        //         this.setState({
+        //             searchInfo: data
+        //         }, function(){
+        //             console.log(this.state.searchInfo)
+        //         })
+        //     }
+        // }
+        // let data = Object.assign({}, this.state.datavalue, {
+        //     name: e.target.value
+        // })
+        this.setState({
+            changeName: e.target.value
+        }, function () {
+            console.log(this.state)
+        })
+    }
+    changeEmailInfo = (e) => {
+        this.setState({
+            changeEmail: e.target.value
+        })
+    }
+    changeRoleInfo = (item, e) => {
+        // console.log(item)
+        // console.log(e)
+        // let data = Object.assign({}, this.state.datavalue, {
+        //     sex: e
+        // })
+        // this.setState({
+        //     searchInfo: Object.assign({}, this.state.searchInfo, {
+        //         sex: e
+        //     })
+        // }, function() {
+        //     console.log(this.state.searchInfo)
+        // })
+        this.setState({
+            changeRole: e
+        })
+    }
+    SelectInfo = () => {
+        console.log(this.searchInfo)
+    }
+    addUser = () => {
+        this.setState({
+            visible: true,
+            operation: '新增',
+            editData: {}
+        });
+    }
+    handleUpdate = (item) => {
+        let id = item;
+        console.log(id)
+        this.setState({
+            visible: true,
+            operation: '更新',
+            editData: item
+        });
+    }
+    hideModal = () => {
+        this.setState({
+            visible: false,
+        });
+    }
+    handleDelete = (item) => {
+        console.log(item)
+        Modal.confirm({
+            title: '确认',
+            content: '您确认要删除此条数据吗？',
+            onOk: () => {
+                message.success('删除成功');
+                // this.request();
+            }
+        })
+    }
+
+}
+
+const WrappedDynamicRule = Form.create({ name: 'dynamic_rule' })(SystemManage);
+
+export default WrappedDynamicRule;
